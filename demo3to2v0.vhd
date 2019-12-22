ec35231b3b575fd7e649c26dfe3ec8684a63a87f
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-22 20:43:39 +0800

    Update demo3to2v0.vhd

--- demo3to2v0.vhd
+++ demo3to2v0.vhd
@@ -1,18 +1,18 @@
 -- file : demo3to2v0.vhd
--- function : example 3-2 on page61 in ORIGINAL
--- device : Cyclone EP1C20F400C8
+-- function : 半加器（根据半加器电路结构写成）
 -- author : ojw
 -- createDate : 2019-09-18
+-- source : example 3-2 on page61 in ORIGINAL
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY demo3to2v0 IS
-	PORT(
-		A  : IN STD_LOGIC;
-		B  : IN STD_LOGIC;
-		SO : OUT STD_LOGIC;
-		CO : OUT STD_LOGIC
+PORT(
+	A  : IN STD_LOGIC;
+	B  : IN STD_LOGIC;
+	SO : OUT STD_LOGIC;
+	CO : OUT STD_LOGIC
 	);
 END ENTITY demo3to2v0;
 
