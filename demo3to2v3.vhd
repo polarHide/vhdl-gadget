58afdc582746f5a782b97f76427509072a00b235
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-22 20:54:40 +0800

    Update demo3to2v3.vhd

--- demo3to2v3.vhd
+++ demo3to2v3.vhd
@@ -1,8 +1,9 @@
 -- file : demo3to2v3.vhd
--- function : example 3-2 on page61 in WHEN-ELSE
+-- function : 半加器（when-else实现，真值表）
 -- device : Cyclone EP1C20F400C8
 -- author : ojw
 -- createDate : 2019-09-22
+-- source : example 3-2 on page61 in WHEN-ELSE
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -10,8 +11,8 @@ USE IEEE.STD_LOGIC_1164.ALL;
 ENTITY demo3to2v3 IS
 	PORT
 	(
-		 A : IN STD_LOGIC;
-		 B : IN STD_LOGIC;
+		A  : IN STD_LOGIC;
+		B  : IN STD_LOGIC;
 		SO : OUT STD_LOGIC;
 		CO : OUT STD_LOGIC
 	);
@@ -20,12 +21,11 @@ END ENTITY demo3to2v3;
 ARCHITECTURE fh3 OF demo3to2v3 IS
 	SIGNAL S, R : STD_LOGIC_VECTOR ( 1 DOWNTO 0 );
 	BEGIN
-		S <= A & B;
-		R <= "00" WHEN S = "00" ELSE
-			 "10" WHEN S = "01" ELSE
-			 "10" WHEN S = "10" ELSE
-			 "01" WHEN S = "11";
-		SO <= R(1);
-		CO <= R(0);
+	S <= A & B;
+	R <= "00" WHEN S = "00" ELSE
+		 "10" WHEN S = "01" ELSE
+		 "10" WHEN S = "10" ELSE
+		 "01" WHEN S = "11";
+	SO <= R(1);
+	CO <= R(0);
 END ARCHITECTURE fh3;
--- WHEN-ELSE donot need PROCESS concept 
