4becb458227799313b7e48c8dcf07a9aa35f26bd
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-22 13:53:54 +0800

    Update mux21a.vhd

--- mux21a.vhd
+++ mux21a.vhd
@@ -1,25 +1,17 @@
+-- file : mux21a.vhd
+-- fuction : s控制2选1输出
+-- author : ojw
+-- createDate : 2019-09-12
+-- source : book example 3-1 on page58
 
--- date: 2019-09-12
--- author: ojw
--- source: book example 3-1 on page58
-
--- FUCTION
--- multiplexer 2 choose 1 : when s='1',y output a;when s='0',y output b
--- PORT
--- a,b : data channel input interface
--- s : channel selection contorl channel terminal
--- y : data output
-
--- LIBRARY WORK;
--- LIBRARY STD;
--- USE STD.STANDARD.ALL;
--- LIBRARY WORK AND STANDARD default open in VHDL
--- here not necessary write ,so comment is allright
-
+-- update : 2019-12-22
+-- updateFor : 调整代码规范，增加中文注释
 
 ENTITY mux21a IS
-	PORT ( a,b,s : IN BIT;
-			   y : OUT BIT );	-- type BIT only two value 0&1,suggest type STD_LOGIC more
+	PORT( 
+		a,b,s : IN BIT;
+		y     : OUT BIT	--BIT型只有0或1，建议多用std_logic
+	);
 END ENTITY mux21a;
 
 ARCHITECTURE bhv OF mux21a IS
