468f100476c107cff30bb2e54d0af381ce9d2533
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-22 20:48:52 +0800

    Update demo3to2v1.vhd

--- demo3to2v1.vhd
+++ demo3to2v1.vhd
@@ -1,8 +1,8 @@
 -- file : demo3to2v1.vhd
--- function : example 3-2 on page61 in IF
--- device : Cyclone EP1C20F400C8
+-- function : 半加器（if-then-elsif实现，真值表）
 -- author : ojw
 -- createDate : 2019-09-22
+-- source : example 3-2 on page61 in IF
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
@@ -17,21 +17,21 @@ ENTITY demo3to2v1 IS
 END ENTITY demo3to2v1;
 
 ARCHITECTURE fh1 OF demo3to2v1 IS
-	SIGNAL S, R : STD_LOGIC_VECTOR ( 1 DOWNTO 0 );
+SIGNAL S, R : STD_LOGIC_VECTOR ( 1 DOWNTO 0 );
+BEGIN
+	S <= A & B;
+	PROCESS ( A,B )
 	BEGIN
-		S <= A & B;
-		PROCESS ( A,B )
-		BEGIN
-			IF ( S = "00" )
-				THEN R <= "00";
-			ELSIF ( S = "01" )
-				THEN R <= "10";
-			ELSIF ( S = "10" )
-				THEN R <= "10";
-			ELSIF ( S = "11" )
-				THEN R <= "01";
-			END IF;
-			SO <= R(1);
-			CO <= R(0);
-		END PROCESS;
+		IF ( S = "00" )
+			THEN R <= "00";
+		ELSIF ( S = "01" )
+			THEN R <= "10";
+		ELSIF ( S = "10" )
+			THEN R <= "10";
+		ELSIF ( S = "11" )
+			THEN R <= "01";
+		END IF;
+		SO <= R(1);
+		CO <= R(0);
+	END PROCESS;
 END ARCHITECTURE fh1;
