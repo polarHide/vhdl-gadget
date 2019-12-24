89f76e0ed3bd27d3a14f239f013cb596b123fb0f
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-24 10:43:13 +0800

    Update coun16b.vhd

--- coun16b.vhd
+++ coun16b.vhd
@@ -1,6 +1,6 @@
 -- file : counter16b.vhd
--- fuction : �޸���5-15
--- 			��λ���COUT���첽���ؿ���LOAD�����������Զ�������16λ����������������16λ�ɿط�Ƶ����
+-- fuction : 修改例5-15
+-----------  进位输出COUT与异步加载控制LOAD相连，构成自动加载型16位二进制数计数器（16位可控分频器）
 -- device : Cyclone EP1C20F400C8
 -- author : ojw
 -- createDate : 2019-10-09
@@ -11,11 +11,10 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY coun16b IS
 	PORT (
-		 CLK,RST,EN,LOAD : IN STD_LOGIC;	
-		 -- CLKʱ���ź������,LOADԤ�ó�ֵʹ�ܶ�:�ߵ�ƽ��Ч,RST�����:�ߵ�ƽ��Ч,EN����ʹ�ܶ�,�ߵ�ƽ��Ч,
-					DATA : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-					DOUT : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-					COUT : OUT STD_LOGIC	--COUT��λ���,������2^16����һ����λ
+		 CLK,RST,EN,LOAD : IN STD_LOGIC;	-- CLK时钟信号输入端,LOAD预置初值使能端:高电平有效,RST清零端:高电平有效,EN计数使能端,高电平有效
+		 DATA 		 	 : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		 DOUT 			 : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+		 COUT			 : OUT STD_LOGIC	--COUT进位输出,计数满2^16产生一个进位
 );
 END coun16b;
 
@@ -24,9 +23,9 @@ BEGIN
 	PROCESS(CLK,RST)
 	VARIABLE Q : STD_LOGIC_VECTOR (15 DOWNTO 0);
 	BEGIN 	
-		IF RST='1' 							-- ��λ1ʱ�������Ĵ�������
+		IF RST='1' 	-- 复位1时，计数寄存器清零
 			THEN Q:=(OTHERS=>'0');
-		ELSIF CLK'EVENT AND CLK='1' THEN	-- �����ش���
+		ELSIF CLK'EVENT AND CLK='1' THEN	-- 上升沿触发
 			IF LOAD='1' THEN Q:=DATA;
 			ELSIF EN='1' THEN Q:=Q+1;
 			ELSE
