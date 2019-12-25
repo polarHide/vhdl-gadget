192d551220cb58515b51a39f4b69ccd09ae8b3b3
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-25 14:35:25 +0800

    Add files via upload

---
+++ sinMif.m
@@ -0,0 +1,29 @@
+F1 = 1; %信号的频率
+Fs = 2^7;  %采样频率
+P1 = 0; %信号初相位0
+N = 2^7; %采样点数128点
+t = [0 : 1/Fs : (N-1)/Fs];  %采样时刻
+ADC = 2^5;  %直流分量
+A = 2^4;  %信号幅度
+s = A*sin(2*pi*F1*t + pi*P1/180) + ADC; %生成信号
+plot(s);  %绘制图形
+fild = fopen('c:/sinMif/sin8bit_128.mif','wt');  %创建mif文件
+% 写入mif文件文件头
+fprintf(fild,'%s\n','WIDTH=8;');    %位宽
+fprintf(fild,'%s\n\n','DEPTH=128;');   %深度
+fprintf(fild,'%s\n','ADDRESS_RADIX=BIN;');  %地址格式
+fprintf(fild,'%s\n\n','DATA_RADIX=BIN;');   %数据格式
+fprintf(fild,'%s\t','CONTENT'); %地址
+fprintf(fild,'%s\n','BEGIN');
+
+for i = 1 : N
+    s2(i) = round(s(i));    %对小数四舍五入取整
+   
+    % addr : data;
+    fprintf(fild,'\t%s\t',dec2bin((i-1),10)); %地址从0开始编码
+    fprintf(fild,'%s\t',':');
+    fprintf(fild,'%s',dec2bin(s2(i),8));
+    fprintf(fild,'%s\n',';');
+end
+fprintf(fild,'%s\n','END');
+
