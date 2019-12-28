24a084b5696e4a368ecf821ad4e2d5cf22c8c365
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-28 13:31:13 +0800

    Update fsmDiretOutput.vhd

--- fsmDiretOutput.vhd
+++ fsmDiretOutput.vhd
@@ -1,5 +1,6 @@
 -- file : fsmDiretOutput.vhd
--- function : demo 10-1 in three process in DiretOutput Coding
+-- function : ç›´æ¥è¾“å‡ºå‹ç¼–ç æ–¹å¼ å®šä¹‰çŠ¶æ€å˜é‡
+-- source : demo 10-1 in three process in DiretOutput Coding
 -- author : ojw
 -- createDate : 2019-11-01
 
@@ -7,30 +8,30 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity fsmDiretOutput is
-	port (	  clk, reset : in STD_LOGIC;
-			state_inputs : in STD_LOGIC_VECTOR(0 to 1);
-			comb_outputs : out INTEGER RANGE 0 to 15
-		  );
+	port(
+		clk, reset   : in STD_LOGIC;
+		state_inputs : in STD_LOGIC_VECTOR(0 to 1);
+		comb_outputs : out INTEGER RANGE 0 to 15
+		);
 end fsmDiretOutput;
 
 architecture bhv of fsmDiretOutput is
-	-- ·½Ê½Ò»£º×´Ì¬»úµÄÊôĞÔ¶¨ÒåÓï¾äÖ¸Ê¾±àÂë·½Ê½£¨Ö±½ÓÊä³öĞÍ±àÂë£©
+	-- æ–¹å¼ä¸€ï¼šçŠ¶æ€æœºçš„å±æ€§å®šä¹‰è¯­å¥æŒ‡ç¤ºç¼–ç æ–¹å¼ï¼ˆç›´æ¥è¾“å‡ºå‹ç¼–ç ï¼‰
 	type FSM_ST is (s0, s1, s2, s3, s4);
 	attribute enum_encoding : string;
 	attribute enum_encoding of FSM_ST : type is "000 010 100 110 111";
 	signal c_st, next_state : FSM_ST;
 
-	-- ·½Ê½¶ş£º¶Ô×´Ì¬ÔªËØ¶¨Òå³£ÊıÊı¾İÀàĞÍ
---	constant s0 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";	-- state_inputs = "00"£¬Çø·ÖÄ©Î²Î»0
---	constant s1 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "010";	-- state_inputs = "01"£¬Çø·ÖÄ©Î²Î»0
---	constant s2 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "100";	-- state_inputs = "10"£¬Çø·ÖÄ©Î²Î»0
---	constant s3 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "110";	-- state_inputs = "11"£¬Çø·ÖÄ©Î²Î»0
---	constant s4 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "111";	-- state_inputs = "/"£¬Çø·ÖÄ©Î²Î»1
+	-- æ–¹å¼äºŒï¼šå¯¹çŠ¶æ€å…ƒç´ å®šä¹‰å¸¸æ•°æ•°æ®ç±»å‹
+--	constant s0 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";	-- state_inputs = "00"ï¼ŒåŒºåˆ†æœ«å°¾ä½0
+--	constant s1 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "010";	-- state_inputs = "01"ï¼ŒåŒºåˆ†æœ«å°¾ä½0
+--	constant s2 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "100";	-- state_inputs = "10"ï¼ŒåŒºåˆ†æœ«å°¾ä½0
+--	constant s3 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "110";	-- state_inputs = "11"ï¼ŒåŒºåˆ†æœ«å°¾ä½0
+--	constant s4 : STD_LOGIC_VECTOR(2 DOWNTO 0) := "111";	-- state_inputs = "/"ï¼ŒåŒºåˆ†æœ«å°¾ä½1
 --	signal c_st, next_state : STD_LOGIC_VECTOR(2 DOWNTO 0);
 	
 begin
-
-	REG : process (reset, clk)	-- Ê±Ğò½ø³Ì
+	REG : process (reset, clk)	-- æ—¶åºè¿›ç¨‹
 	begin
 		if reset = '0' then
 			c_st <= s0;
@@ -39,7 +40,7 @@ begin
 		end if;
 	end process REG;
 	
-	COM1 : process (c_st, state_inputs)		-- state¿ØÖÆ
+	COM1 : process (c_st, state_inputs)		-- stateæ§åˆ¶
 	begin
 		case c_st is
 			when s0 => 	if state_inputs = "00" then next_state <= s0;
@@ -59,7 +60,7 @@ begin
 		end case;
 	end process COM1;
 	
-	COM2 : process (c_st)	-- Êä³ö¿ØÖÆ
+	COM2 : process (c_st)	-- è¾“å‡ºæ§åˆ¶
 	begin
 		case c_st is
 			when s0 => comb_outputs <= 5;	-- 0101						
