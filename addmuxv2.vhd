ab248b292719aadfea595934b785a1581b671b85
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-29 16:02:57 +0800

    Update addmuxv2.vhd

--- addmuxv2.vhd
+++ addmuxv2.vhd
@@ -9,27 +9,27 @@ use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 entity addmuxv2 is
-	port (
-			R 	   : out std_logic_vector(7 downto 0);
-			sel 	   : in std_logic;
-			A, B, C, D : in std_logic_vector(7 downto 0)
-		  );
+    port (
+         R          : out std_logic_vector(7 downto 0);
+         sel        : in std_logic;
+         A, B, C, D : in std_logic_vector(7 downto 0)
+         );
 end addmuxv2;
 
 architecture rtl of addmuxv2 is
-	signal temp0 : std_logic_vector(7 downto 0);
-	signal temp1 : std_logic_vector(7 downto 0);
+        signal temp0 : std_logic_vector(7 downto 0);
+        signal temp1 : std_logic_vector(7 downto 0);
 begin
-	process(A, B, C, D, sel)
-	begin
-		if(sel = '0') then
-		temp0 <= A;
-		temp1 <= B;
-		R <= temp0 + temp1;
-		else
-		temp0 <= C;
-		temp1 <= D;
-		R <= temp0 + temp1;
-		end if;
-	end process;
+        process(A, B, C, D, sel)
+        begin
+        if(sel = '0') then
+          temp0 <= A;
+          temp1 <= B;
+          R <= temp0 + temp1;
+        else
+          temp0 <= C;
+          temp1 <= D;
+          R <= temp0 + temp1;
+        end if;
+        end process;
 end rtl;
