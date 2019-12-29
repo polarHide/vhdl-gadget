0e946bf4fe9158b1e70b0a9a2711349a9ca12e91
polarHide <51467001+polarHide@users.noreply.github.com>
2019-12-29 16:04:24 +0800

    Update addmux.vhd

--- addmux.vhd
+++ addmux.vhd
@@ -9,21 +9,21 @@ use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
 entity addmux is
-	port (
-			R : out std_logic_vector(7 downto 0);
-			sel : in std_logic;
-			A, B, C, D : in std_logic_vector(7 downto 0)
-		  );
+    port (
+            R          : out std_logic_vector(7 downto 0);
+            sel        : in std_logic;
+            A, B, C, D : in std_logic_vector(7 downto 0)
+        );
 end addmux;
 
 architecture rtl of addmux is
 begin
-	process(A, B, C, D, sel)
-	begin
-		if(sel = '0') then
-		R <= A + B;
-		else
-		R <= C + D;
-		end if;
+        process(A, B, C, D, sel)
+        begin
+        if(sel = '0') then
+            R <= A + B;
+        else
+            R <= C + D;
+        end if;
 	end process;
 end rtl;
